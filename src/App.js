6266eb3aad6df000da7293c9725620a0b5ab0301
Emanuel Restrepo <emavdavresgar@gmail.com>
2022-03-01 23:03:54 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
